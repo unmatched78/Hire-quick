e3be972cd34e2f03f2170f3d4f2a25658cd0a484
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-01 10:00:40 +0000

    urls

--- hireme/core/urls.py
+++ hireme/core/urls.py
@@ -1,1 +1,15 @@
-si
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, JobSeekerViewSet, CompanyViewSet, JobViewSet, ApplicationViewSet, ReviewViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'jobseekers', JobSeekerViewSet)
+router.register(r'companies', CompanyViewSet)
+router.register(r'jobs', JobViewSet)
+router.register(r'applications', ApplicationViewSet)
+router.register(r'reviews', ReviewViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- hireme/core/views.py
+++ hireme/core/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import User, JobSeeker, Company, Job, Application, Review
+from .serializers import UserSerializer, JobSeekerSerializer, CompanySerializer, JobSerializer, ApplicationSerializer, ReviewSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class JobSeekerViewSet(viewsets.ModelViewSet):
+    queryset = JobSeeker.objects.all()
+    serializer_class = JobSeekerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class JobViewSet(viewsets.ModelViewSet):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class ApplicationViewSet(viewsets.ModelViewSet):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticated]
