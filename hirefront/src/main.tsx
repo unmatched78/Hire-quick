632cd9938379656dc2b59020728e96226ca927d1
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-01 10:24:14 +0000

    maintsx

--- hirefront/src/main.tsx
+++ hirefront/src/main.tsx
@@ -1,10 +1,14 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from './theme';
 import App from './App';
-import './index.css'
+import './index.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ChakraProvider theme={theme}>
-    <App />
-  </ChakraProvider>
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
 );
