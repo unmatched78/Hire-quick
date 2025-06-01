a903a45ea9432782e4a1740b0596fd9104952598
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-01 18:22:04 +0000

    landingpage

--- recruit/src/App.tsx
+++ recruit/src/App.tsx
@@ -1,16 +1,17 @@
-import HeroSection from '@/components/HeroSection';
-import FeaturesSection from '@/components/FeaturesSection';
-import TestimonialsSection from '@/components/TestimonialsSection';
-import CTASection from '@/components/CTASection';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from '@/pages/LandingPage';
+import Navbar from '@/components/Navbar';
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <HeroSection />
-      <FeaturesSection />
-      <TestimonialsSection />
-      <CTASection />
-    </div>
+    <Router>
+      <div className="min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
