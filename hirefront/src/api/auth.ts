ccd5cf3444536dd1744703bc79174bc36a146a2e
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-01 10:57:15 +0000

    baseurl

--- hirefront/src/api/auth.ts
+++ hirefront/src/api/auth.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const login = async (username: string, password: string) => {
-  const response = await axios.post('http://your-backend-url/api/token/', { username, password });
+  const response = await axios.post('http://localhost:8000/api/token/', { username, password });
   localStorage.setItem('token', response.data.token);
   return response.data;
 };
--- hirefront/src/api/jobs.ts
+++ hirefront/src/api/jobs.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const getJobs = async () => {
-  const response = await axios.get('http://your-backend-url/api/jobs/');
+  const response = await axios.get('http://localhost:8000/api/jobs/');
   return response.data;
 };
