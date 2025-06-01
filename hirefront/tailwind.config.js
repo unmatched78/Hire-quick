2f22622b0bdb7898bc12607ff83e45da5669d35d
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-01 11:11:44 +0000

    remove flowbite

--- hirefront/tailwind.config.js
+++ hirefront/tailwind.config.js
@@ -1,7 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Include all source files
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          500: '#7928CA', // Neon purple
+          600: '#FF0080', // Neon pink
+          700: '#00DDEB', // Neon cyan
+        },
+      },
+    },
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [], // Remove flowbite/plugin
 };
