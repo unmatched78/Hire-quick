2b70031ebc8058fa6e849e5b57ddeabbe2dbb820
Viella <168560797+unmatched78@users.noreply.github.com>
2025-07-21 19:01:12 +0000

    clean

--- companies/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Company
-
-@admin.register(Company)
-class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'industry', 'size', 'location', 'created_at')
-    search_fields = ('name', 'industry', 'location')
-    list_filter = ('industry', 'size', 'created_at')
-    readonly_fields = ('created_at', 'updated_at')
--- companies/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'companies'
-
-urlpatterns = [
-    path('', views.CompanyListView.as_view(), name='list'),
-    path('<int:pk>/', views.CompanyDetailView.as_view(), name='detail'),
-]
